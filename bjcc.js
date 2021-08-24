e9e199d4cceae7989c1c8d94aebabbe3ed73602f
Yurihaia <17830663+Yurihaia@users.noreply.github.com>
2021-08-23 20:36:06 -0400

    add stuff

---
+++ bjcc.js
@@ -0,0 +1,23 @@
+let num = 0;
+const phases = [
+    "Initial Positions",
+    "Optical Sight Bait",
+    "First Lightning + Water",
+    "Missile Command",
+    "Second Lightning + Water",
+    "Limit Cut + Flarethrower",
+    "Third Lightning + Water",
+    "Gavel"
+];
+
+document.onkeypress = e => {
+    e = e || window.event;
+    if (e.key == "d") {
+        num = (num + 1) % 8;
+    } else if (e.key == "a") {
+        if (num == 0) num = 7;
+        else num -= 1;
+    }
+    document.getElementById("slide").src = `bjcc${num}.png`;
+    document.getElementById("phase").innerText = phases[num];
+};
